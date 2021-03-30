c58ff000915dc2c218520c37761c0573ae6a4777
Basel Alaktaa <basel.alaktaa.94@gmail.com>
2021-03-30 20:46:07 +0200

    added

--- summe-practice/run.sh
+++ summe-practice/run.sh
@@ -1,4 +1,8 @@
-echo "class Summe { public int summe (int a , int b){ $answer } }" > .C:/Users/Basel Alaktaa/Desktop/BSC/first-Step/summe_pracitce/summe-practice/src/main/java/summe
+
+
+
+
+echo "class Summe { public int summe (int a , int b){ $answer } }" > .C:/Users/Basel Alaktaa/Desktop/BSC/first-Step/summe_pracitce/summe-practice/src/main/java/summe/Summe.java
 
 
 
